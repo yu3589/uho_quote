ab1a91ff09f8a3e0657f0b0e171118cfbebaf66f
yu3589 <uegtz34714@yahoo.co.jp>
2025-05-01 15:02:31 +0900

    挙動確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # 挙動確認
   allow_browser versions: :modern
 end
