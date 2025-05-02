5f06af8c01dd278728a817f4cc45a230c8d3defb
yu3589 <uegtz34714@yahoo.co.jp>
2025-05-02 14:44:07 +0900

    add:トップページ実装 #7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "posts/index"
+  root "quotes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "posts#index"
+  # root "posts#index"
 end
