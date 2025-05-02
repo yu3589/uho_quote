9383217cfa128701c81b7b6694264c7649c82024
yu3589 <uegtz34714@yahoo.co.jp>
2025-05-02 20:24:41 +0900

    add:カテゴリー画面への遷移機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "quotes#top"
+  get "quotes/category"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
