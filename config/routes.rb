ea834489d6f93d78562e35a3c08798b8e0344dfe
yu3589 <uegtz34714@yahoo.co.jp>
2025-05-07 09:27:42 +0900

    add:名言表示画面への遷移追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "quotes#top"
-  get "category", to: "quotes#category", as: "category"
+  get "categories", to: "quotes#category", as: "categories"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/categories/:category', to: 'quotes#show', as: :category_quote
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
