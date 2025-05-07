b0803d61408ddd41f0b7662b7af3a40de6ec9b45
yu3589 <uegtz34714@yahoo.co.jp>
2025-05-08 01:29:57 +0900

    add:投稿機能追加追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  # 挙動確認
   allow_browser versions: :modern
+  helper ApplicationHelper
 end
