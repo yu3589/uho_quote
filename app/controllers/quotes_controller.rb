0c2610f23a39e6ddffbad04a9a2b5988c66e5282
yu3589 <uegtz34714@yahoo.co.jp>
2025-05-07 09:26:14 +0900

    add:名言表示機能追加

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,19 @@
 class QuotesController < ApplicationController
-  def category; end
+  require 'yaml'
+  before_action :set_quote
+
+  def category;
+  end
+
+  def show
+    category = params[:category]
+    @category = params[:category]
+    @quote = @quotes[category]&.sample
+  end
+
+  private
+
+  def set_quote
+    @quotes = YAML.load_file(Rails.root.join('config', "uho_quote.yml"))
+  end
 end
