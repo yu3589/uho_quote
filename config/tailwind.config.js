0c1f6d8bec22bd68f99936296947235d6f174c1e
yu3589 <uegtz34714@yahoo.co.jp>
2025-05-09 12:45:54 +0900

    fix:変更取り消し

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.{html,erb}',
+    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}'
