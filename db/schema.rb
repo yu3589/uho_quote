ea3f5bf6865aaa931842d498234723a53670487b
yu3589 <uegtz34714@yahoo.co.jp>
2025-07-13 16:32:50 +0900

    add :UsersテーブルにOmniAuth用の一意カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_10_080357) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_13_065911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_10_080357) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 end
