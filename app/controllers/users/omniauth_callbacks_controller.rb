23798e8a4309417a99ad51c4d137e7427383cd19
yu3589 <uegtz34714@yahoo.co.jp>
2025-07-14 19:06:35 +0900

    feat: ログイン失敗時のフラッシュメッセージを追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,12 +17,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except(:extra)
-      redirect_to new_user_registration_url
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
 
   def failure
-    redirect_to root_path
+    redirect_to root_path, alert: "認証に失敗しました。もう一度お試しください。"
   end
 
   # protected
